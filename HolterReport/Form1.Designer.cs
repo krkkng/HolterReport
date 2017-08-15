15ed82a7bdce7e3d7b5b3af698768ada7958d3a8
krk <krkkng@outlook.jp>
2017-08-15 11:38:44 +0000

    first commit

---
+++ HolterReport/Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace HolterReport
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 必要なデザイナー変数です。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 使用中のリソースをすべてクリーンアップします。
+		/// </summary>
+		/// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows フォーム デザイナーで生成されたコード
+
+		/// <summary>
+		/// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+		/// コード エディターで変更しないでください。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.Windows.Forms.Button button1;
+			this.txtmain = new System.Windows.Forms.TextBox();
+			this.txt1 = new System.Windows.Forms.TextBox();
+			this.txt2 = new System.Windows.Forms.TextBox();
+			this.txt3 = new System.Windows.Forms.TextBox();
+			this.txt4 = new System.Windows.Forms.TextBox();
+			this.txt5 = new System.Windows.Forms.TextBox();
+			this.btn1 = new System.Windows.Forms.Button();
+			this.btn2 = new System.Windows.Forms.Button();
+			this.btn3 = new System.Windows.Forms.Button();
+			this.btn4 = new System.Windows.Forms.Button();
+			this.btn5 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			button1 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			button1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			button1.Location = new System.Drawing.Point(205, 192);
+			button1.Name = "button1";
+			button1.Size = new System.Drawing.Size(75, 23);
+			button1.TabIndex = 6;
+			button1.Text = "作成";
+			button1.UseVisualStyleBackColor = true;
+			button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// txtmain
+			// 
+			this.txtmain.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.txtmain.Location = new System.Drawing.Point(12, 12);
+			this.txtmain.Multiline = true;
+			this.txtmain.Name = "txtmain";
+			this.txtmain.Size = new System.Drawing.Size(447, 174);
+			this.txtmain.TabIndex = 0;
+			this.txtmain.TextChanged += new System.EventHandler(this.txtmain_TextChanged);
+			// 
+			// txt1
+			// 
+			this.txt1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.txt1.Location = new System.Drawing.Point(12, 224);
+			this.txt1.Name = "txt1";
+			this.txt1.Size = new System.Drawing.Size(399, 19);
+			this.txt1.TabIndex = 1;
+			// 
+			// txt2
+			// 
+			this.txt2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.txt2.Location = new System.Drawing.Point(12, 249);
+			this.txt2.Name = "txt2";
+			this.txt2.Size = new System.Drawing.Size(399, 19);
+			this.txt2.TabIndex = 2;
+			// 
+			// txt3
+			// 
+			this.txt3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.txt3.Location = new System.Drawing.Point(12, 274);
+			this.txt3.Name = "txt3";
+			this.txt3.Size = new System.Drawing.Size(399, 19);
+			this.txt3.TabIndex = 3;
+			// 
+			// txt4
+			// 
+			this.txt4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.txt4.Location = new System.Drawing.Point(12, 299);
+			this.txt4.Name = "txt4";
+			this.txt4.Size = new System.Drawing.Size(399, 19);
+			this.txt4.TabIndex = 4;
+			// 
+			// txt5
+			// 
+			this.txt5.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.txt5.Location = new System.Drawing.Point(12, 324);
+			this.txt5.Name = "txt5";
+			this.txt5.Size = new System.Drawing.Size(399, 19);
+			this.txt5.TabIndex = 5;
+			// 
+			// btn1
+			// 
+			this.btn1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.btn1.Location = new System.Drawing.Point(417, 222);
+			this.btn1.Name = "btn1";
+			this.btn1.Size = new System.Drawing.Size(42, 23);
+			this.btn1.TabIndex = 7;
+			this.btn1.Text = "copy";
+			this.btn1.UseVisualStyleBackColor = true;
+			this.btn1.Click += new System.EventHandler(this.btn1_Click);
+			// 
+			// btn2
+			// 
+			this.btn2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.btn2.Location = new System.Drawing.Point(417, 247);
+			this.btn2.Name = "btn2";
+			this.btn2.Size = new System.Drawing.Size(42, 23);
+			this.btn2.TabIndex = 8;
+			this.btn2.Text = "copy";
+			this.btn2.UseVisualStyleBackColor = true;
+			this.btn2.Click += new System.EventHandler(this.btn2_Click);
+			// 
+			// btn3
+			// 
+			this.btn3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.btn3.Location = new System.Drawing.Point(417, 272);
+			this.btn3.Name = "btn3";
+			this.btn3.Size = new System.Drawing.Size(42, 23);
+			this.btn3.TabIndex = 9;
+			this.btn3.Text = "copy";
+			this.btn3.UseVisualStyleBackColor = true;
+			this.btn3.Click += new System.EventHandler(this.btn3_Click);
+			// 
+			// btn4
+			// 
+			this.btn4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.btn4.Location = new System.Drawing.Point(417, 297);
+			this.btn4.Name = "btn4";
+			this.btn4.Size = new System.Drawing.Size(42, 23);
+			this.btn4.TabIndex = 10;
+			this.btn4.Text = "copy";
+			this.btn4.UseVisualStyleBackColor = true;
+			this.btn4.Click += new System.EventHandler(this.btn4_Click);
+			// 
+			// btn5
+			// 
+			this.btn5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.btn5.Location = new System.Drawing.Point(417, 322);
+			this.btn5.Name = "btn5";
+			this.btn5.Size = new System.Drawing.Size(42, 23);
+			this.btn5.TabIndex = 11;
+			this.btn5.Text = "copy";
+			this.btn5.UseVisualStyleBackColor = true;
+			this.btn5.Click += new System.EventHandler(this.btn5_Click);
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(12, 192);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(75, 23);
+			this.button2.TabIndex = 12;
+			this.button2.Text = "削除";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(391, 192);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(35, 12);
+			this.label1.TabIndex = 13;
+			this.label1.Text = "label1";
+			this.label1.Click += new System.EventHandler(this.label1_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(471, 357);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.btn5);
+			this.Controls.Add(this.btn4);
+			this.Controls.Add(this.btn3);
+			this.Controls.Add(this.btn2);
+			this.Controls.Add(this.btn1);
+			this.Controls.Add(button1);
+			this.Controls.Add(this.txt5);
+			this.Controls.Add(this.txt4);
+			this.Controls.Add(this.txt3);
+			this.Controls.Add(this.txt2);
+			this.Controls.Add(this.txt1);
+			this.Controls.Add(this.txtmain);
+			this.Name = "Form1";
+			this.Text = "ホルターレポート作成支援";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox txtmain;
+		private System.Windows.Forms.TextBox txt1;
+		private System.Windows.Forms.TextBox txt2;
+		private System.Windows.Forms.TextBox txt3;
+		private System.Windows.Forms.TextBox txt4;
+		private System.Windows.Forms.TextBox txt5;
+		private System.Windows.Forms.Button btn1;
+		private System.Windows.Forms.Button btn2;
+		private System.Windows.Forms.Button btn3;
+		private System.Windows.Forms.Button btn4;
+		private System.Windows.Forms.Button btn5;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Label label1;
+	}
+}
+
---
+++ HolterReport/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HolterReport
+{
+	public partial class Form1 : Form
+	{
+		string[] strs = new string[5] { "", "", "", "", "" };
+		public Form1()
+		{
+			InitializeComponent();
+			this.TopMost = true;
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			txt1.Text = "";
+			txt2.Text = "";
+			txt3.Text = "";
+			txt4.Text = "";
+			txt5.Text = "";
+			strs[0] = "";
+			strs[1] = "";
+			strs[2] = "";
+			strs[3] = "";
+			strs[4] = "";
+			Encoding enc = Encoding.GetEncoding("Shift_JIS");
+			int num = enc.GetByteCount(txtmain.Text);
+			if (num > 64 * 5)
+				MessageBox.Show("文字数オーバー");
+
+			string[] strstmp = mbStrSplit(txtmain.Text, 64);
+			for (int i = 0; i < strstmp.Count(); i++ )
+			{
+				strs[i] = strstmp[i];
+			}
+			txt1.Text = strs[0];
+			txt2.Text = strs[1];
+			txt3.Text = strs[2];
+			txt4.Text = strs[3];
+			txt5.Text = strs[4];
+			
+
+		}
+		//*********************************************************************
+		/// <summary>文字列を指定した文字数単位で分割する(全角文字考慮)
+		/// </summary>
+		/// <param name="inStr">  分割前文字列</param>
+		/// <param name="length"> 1行の長さ</param>
+		/// <returns>             分割後文字列の配列</returns>
+		//*********************************************************************
+		private string[] mbStrSplit(string inStr, int length)
+		{
+			List<string> outArray = new List<string>(); // 分割結果の保存領域
+			string outStr = "";                 // 現在処理中の分割後文字列
+			Encoding enc = Encoding.GetEncoding("Shift_JIS");
+
+
+			// パラメータチェック
+			if (inStr == null || length < 1)
+			{
+				return outArray.ToArray();
+			}
+
+			//--------------------------------------
+			// 全ての文字を処理するまで繰り返し
+			//--------------------------------------
+			for (int offset = 0; offset < inStr.Length; offset++)
+			{
+				//----------------------------------------------------------
+				// 今回処理する文字と、その文字を含めた分割後文字列長を取得
+				//----------------------------------------------------------
+				string curStr = inStr[offset].ToString();
+				int curTotalLength = enc.GetByteCount(outStr) + enc.GetByteCount(curStr);
+
+				//-------------------------------------
+				// この文字が、分割点になるかチェック
+				//-------------------------------------
+				if (curTotalLength == length)
+				{
+					// 処理中の文字を含めると、ちょうどピッタリ
+					outArray.Add(outStr + curStr);
+					outStr = "";
+				}
+				else if (curTotalLength > length)
+				{
+					// 処理中の文字を含めると、あふれる
+					outArray.Add(outStr);
+					outStr = curStr;
+				}
+				else
+				{
+					// 処理中の文字を含めてもまだ余裕あり
+					outStr += curStr;
+				}
+			}
+
+			// 最後の行の文を追加する
+			if (!outStr.Equals(""))
+			{
+				outArray.Add(outStr);
+			}
+
+			// 分割後データを配列に変換して返す
+			return outArray.ToArray();
+		}
+
+		private void btn1_Click(object sender, EventArgs e)
+		{
+			Clipboard.SetText(txt1.Text);
+		}
+
+		private void btn2_Click(object sender, EventArgs e)
+		{
+			Clipboard.SetText(txt2.Text);
+		}
+
+		private void btn3_Click(object sender, EventArgs e)
+		{
+			Clipboard.SetText(txt3.Text);
+		}
+
+		private void btn4_Click(object sender, EventArgs e)
+		{
+			Clipboard.SetText(txt4.Text);
+		}
+
+		private void btn5_Click(object sender, EventArgs e)
+		{
+			Clipboard.SetText(txt5.Text);
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			txtmain.Text = "";
+			txt1.Text = "";
+			txt2.Text = "";
+			txt3.Text = "";
+			txt4.Text = "";
+			txt5.Text = "";
+		}
+
+		private void txtmain_TextChanged(object sender, EventArgs e)
+		{
+			Encoding enc = Encoding.GetEncoding("Shift_JIS");
+			int num = enc.GetByteCount(txtmain.Text);
+
+			label1.Text = "あと" + (64 * 5 - num).ToString() + "byte";
+		}
+
+		private void label1_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			label1.Text = "あと" + (64 * 5).ToString() + "byte";
+		}
+	}
+
+
+}
